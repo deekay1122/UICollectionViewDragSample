b01cc82bae5933298800eb837f88cf2f6b5377cd
deekay1122 <deekay1122@gmail.com>
2022-11-11 11:16:33 +0900

    Initial Commit

---
+++ CollectionViewDragPragtice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewDragPragtice
+//
+//  Created by Daisaku Ejiri on 2022/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
